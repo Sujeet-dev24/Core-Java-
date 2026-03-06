7e2a5923101041ac80b9e1c101d2b14eff031edc
Sujeet-dev24 <sgk22@SUJEETgmail.com>
2026-03-06 20:25:50 +0530

    logical operator

---
+++ Logical/LogicalOperators.java
@@ -0,0 +1,18 @@
+public class LogicalOperators {
+    public static void main(String [] args){
+
+        //logical operators
+        int a = 15;
+        int b = 20;
+        int c = 25;
+
+        
+        boolean d = (a<b) && (b<c);
+
+        //short circuit
+
+            System.out.println(d);// true
+        }
+    }
+    
+
